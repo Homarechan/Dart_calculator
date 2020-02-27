ab498f0693eb28e8cfb43e66611e5984790228d5
Homarechan <tikuwakunn@icloud.com>
2020-02-27 21:41:25 +0900

    Add test code

---
+++ test/tokenizer_test.dart
@@ -0,0 +1,17 @@
+import 'package:test/test.dart';
+
+import '../lib/tokenizer.dart';
+import '../lib/tokens.dart';
+
+main() {
+  test("Tokenizer.tokenize() tokenize the string to list of TokenInstance", () {
+    String string = "1 + 1";
+    Tokenizer tokenizer = Tokenizer(string);
+    tokenizer.tokenize();
+    expect(tokenizer.tokenlist, [
+      TokenInstance(Token.Number, "1", 1, 1),
+      TokenInstance(Token.Operator, "+", 0, 1),
+      TokenInstance(Token.Number, "1", 1, 1)
+    ]);
+  });
+}
