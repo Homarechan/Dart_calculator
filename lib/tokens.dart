aed33f5c55d48ebbc672f922d1e673412822a1d6
Homarechan <tikuwakunn@icloud.com>
2020-02-27 20:51:26 +0900

    Implement TokenInstance class

--- lib/tokens.dart
+++ lib/tokens.dart
@@ -1,4 +1,9 @@
-enum Token {
-  Number,
-  Operator
+enum Token { Number, Operator }
+
+class TokenInstance {
+  Token token;
+  String str;
+  int length;
+
+  TokenInstance(this.token, this.str, this.length);
 }
